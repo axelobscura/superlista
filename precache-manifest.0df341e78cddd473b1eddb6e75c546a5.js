134f4e4502a2393e0c9bdafbc0d228d06c1e88b0
Axel <axosar@gmail.com>
2019-12-02 15:53:40 -0600

    Updates

---
+++ precache-manifest.0df341e78cddd473b1eddb6e75c546a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2859e00d6446f926315984f8687eb516",
+    "url": "/superlista/index.html"
+  },
+  {
+    "revision": "074116fa0aac628dad93",
+    "url": "/superlista/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4a45404b5263e8f50a87",
+    "url": "/superlista/static/js/2.9eddbd47.chunk.js"
+  },
+  {
+    "revision": "074116fa0aac628dad93",
+    "url": "/superlista/static/js/main.9928f23a.chunk.js"
+  },
+  {
+    "revision": "a6734081df24c23524b7",
+    "url": "/superlista/static/js/runtime-main.4504bdf0.js"
+  }
+]);
