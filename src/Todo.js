0d4059484055f2d638ffb29ca0222fb81843eedf
Axel <axosar@gmail.com>
2019-12-11 11:40:16 -0600

    Adding the memo function

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, memo } from 'react';
 import useToggleState from './hooks/useToggleState';
 import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
@@ -13,6 +13,7 @@ import { DispatchContext } from './context/todos.context';
 function Todo({ id, task, completed }){
   const dispatch = useContext(DispatchContext);
   const [isEditing, toggle] = useToggleState(false);
+  console.log("todo re-render: ", task);
   return (
     <ListItem style={{ height: '64px' }}>
       {isEditing ? (
@@ -41,4 +42,4 @@ function Todo({ id, task, completed }){
   )
 }
 
-export default Todo;
+export default memo(Todo);
